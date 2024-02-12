1d295b0303530e9d436ae7eb29052bd3bd427b4f
Prasanna V <you@example.com>
2024-02-12 11:03:30 +0530

    coded

---
+++ props-assignment/src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css';
+// import elephant from "./images/elephant.jpeg";
+import "./style.css"
+
+function App(props) {
+  // code here
+  const out = props.name;
+  return (<div >
+    <h1 className='text'>Kalvium Gallary</h1>
+    <div className='main-container'>
+      {
+      out.map((element, index, out)=>{
+        return <div key={element.id}>
+          <img src={element.img} />
+        </div>
+      })
+    }
+    </div>
+  </div>)
+  
+}
+
+export default App;
---
+++ props-assignment/src/AppClass.jsx
@@ -0,0 +1,26 @@
+import { Component } from 'react';
+import './App.css';
+
+export default class AppClass extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h1 className="text">Kalvium Gallary</h1>
+        <div className="main-container">
+          {this.props.name.map(function (element, index, out) {
+            return (
+              <div key={element.id}>
+                <img src={element.img} />
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    );
+  }
+
+  // code here
+}
